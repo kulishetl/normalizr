d1fd617658ec879cc464b1a30129a47f7a894f4c
kulishetl <kulishetl@gmail.com>
2018-06-21 19:48:24 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,123 @@
+import { schema, normalize } from "normalizr";
+
+const instructorSchema = new schema.Entity('instructors');
+
+const courseSchema = new schema.Entity('courses', {
+  instructors: [instructorSchema],
+});
+
+const studentSchema = new schema.Entity('students', {
+  courses: [courseSchema],
+  instructors: [instructorSchema],
+});
+
+
+
+const data = [
+  {
+    id: "student-1",
+    name: "Mango",
+    courses: [
+      {
+        id: "course-1",
+        name: "Blockchain",
+        instructors: [
+          {
+            id: "instructor-1",
+            name: "Jack"
+          },
+          {
+            id: "instructor-2",
+            name: "Ming"
+          }
+        ]
+      },
+      {
+        id: "course-2",
+        name: "AI",
+        instructors: [
+          {
+            id: "instructor-2",
+            name: "Ming"
+          },
+          {
+            id: "instructor-3",
+            name: "Pam"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "student-2",
+    name: "Poly",
+    courses: [
+      {
+        id: "course-2",
+        name: "AI",
+        instructors: [
+          {
+            id: "instructor-2",
+            name: "Ming"
+          },
+          {
+            id: "instructor-3",
+            name: "Pam"
+          }
+        ]
+      },
+      {
+        id: "course-3",
+        name: "Machine Learning",
+        instructors: [
+          {
+            id: "instructor-1",
+            name: "Jack"
+          },
+          {
+            id: "instructor-4",
+            name: "David"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "student-3",
+    name: "Ajax",
+    courses: [
+      {
+        id: "course-1",
+        name: "Blockchain",
+        instructors: [
+          {
+            id: "instructor-1",
+            name: "Jack"
+          },
+          {
+            id: "instructor-2",
+            name: "Ming"
+          }
+        ]
+      },
+      {
+        id: "course-3",
+        name: "Machine Learning",
+        instructors: [
+          {
+            id: "instructor-1",
+            name: "Jack"
+          },
+          {
+            id: "instructor-4",
+            name: "David"
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const normalizedData = normalize(data, [studentSchema]);
+
+console.log("normalizedData: ", normalizedData);
